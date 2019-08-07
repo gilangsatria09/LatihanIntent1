52736849b9c4139efcc822deb4ccaf30861aa8be
Gilang Satria <gsatria253@gmail.com>
2019-08-07 11:05:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intent1/IntentJava.java
@@ -0,0 +1,13 @@
+package com.example.intent1;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class IntentJava extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+}
